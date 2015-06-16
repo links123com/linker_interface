eb27d62164d40354b6538acec3de859e37d8d3ba
Paul <paul@links123.cn>
2015-06-17 00:03:04 +0800

    [M] 排除下拉时有重复数据的可能

--- app/Logic/Timeline/ReadForm.php
+++ app/Logic/Timeline/ReadForm.php
@@ -9,8 +9,8 @@ class ReadForm extends Validator
         $validator = Validator::make($data, [
             'user_id'         => 'required|integer|min:1',
             'last_pull'       => 'required_with:toward|integer|min:1431705600',
-            'page'            => 'required_with:toward|integer|min:1',
-            'toward'          => 'string|in:up'
+            'page'            => 'integer|min:1',
+            'toward'          => 'string|in:up,down'
         ]);
 
         if($validator->fails()) {
@@ -25,8 +25,14 @@ class ReadForm extends Validator
     {
         $temp = ['user_id' => intval($data['user_id'])];
 
-        if(isset($data['toward']) && $data['toward'] == 'up') {
-            $temp['create_at'] = array('$lte' => intval($data['last_pull']));
+        if(isset($data['toward'])) {
+            if($data['toward'] == 'up') {
+                $temp['create_at'] = array('$lte' => intval($data['last_pull']));
+            }
+
+            if($data['toward'] == 'down') {
+                $temp['create_at'] = array('$gte' => intval($data['last_pull']));
+            }
         }
 
         return $temp;
