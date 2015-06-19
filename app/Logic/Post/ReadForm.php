163abe36f222a047ffba5f52007adde2e0e387f8
Paul <paul@links123.cn>
2015-06-19 10:13:23 +0800

    [M] 修改读取校友

--- app/Logic/Post/ReadForm.php
+++ app/Logic/Post/ReadForm.php
@@ -7,10 +7,11 @@ class ReadForm extends Validator
     public static function validate($data)
     {
         $validator = Validator::make($data, [
-            'user_id'         => 'required_without_all:high_school,middle_school,primary_school|integer|min:1',
-            'high_school'     => 'required_without_all:user_id,middle_school,primary_school|string|min:1',
-            'middle_school'   => 'required_without_all:user_id,high_school,primary_school|string|min:1',
-            'primary_school'  => 'required_without_all:user_id,high_school,middle_school|string|min:1',
+            'user_id'         => 'required_without_all:university,high_school,middle_school,primary_school|integer|min:1',
+            'university'      => 'required_without_all:user_id,high_school,middle_school,primary_school|string|min:1',
+            'high_school'     => 'required_without_all:user_id,university,middle_school,primary_school|string|min:1',
+            'middle_school'   => 'required_without_all:user_id,university,high_school,primary_school|string|min:1',
+            'primary_school'  => 'required_without_all:user_id,university,high_school,middle_school|string|min:1',
             'page'            => 'required|integer|min:1',
             'last_pull'       => 'required_with:toward|integer|min:1431705600',
             'toward'          => 'required|string|in:up,down'
@@ -29,6 +30,10 @@ class ReadForm extends Validator
         $temp = [];
         if(isset($data['user_id'])) {
             $temp['user_id'] = intval($data['user_id']);
+        }
+
+        if(isset($data['university'])) {
+            $temp['university'] = $data['university'];
         }
 
         if(isset($data['high_school'])) {
