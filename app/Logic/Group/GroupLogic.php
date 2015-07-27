c15fd8fc9fd829a83aded3ebde38d0949997436f
Paul <paul@links123.cn>
2015-07-27 23:23:35 +0800

    [M] 添加群中model换位logic

--- app/Logic/Group/GroupLogic.php
+++ app/Logic/Group/GroupLogic.php
@@ -1,6 +1,5 @@
 <?php namespace App\Logic\Group;
 
-use App\Models\GroupMemberModel;
 use App\Models\GroupModel;
 
 class GroupLogic
@@ -14,7 +13,7 @@ class GroupLogic
         // todo:软事物处理
         if($result) {
             // 把群主添加到群中
-            GroupMemberModel::insert(['user_id'=>$data['owner'], 'group_id'=>strval($result)]);
+            GroupMemberLogic::create(['user_id'=>$data['owner'], 'group_id'=>strval($result)]);
         }
 
         return $result;
